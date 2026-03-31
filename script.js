f8a63f77a91ba21cea6db455a15853a66617b835
phurley66 <paulajosephinek@gmail.com>
2026-03-30 23:35:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,21 +6,16 @@ window.addEventListener('scroll', function() {
     
     sections.forEach(function(section) {
         const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
         
         if (window.scrollY >= sectionTop - 300) {
             current = section.getAttribute('id');
         }
     });
 
-    // Debug - show current section
-    console.log('Current section:', current);
-
     navLinks.forEach(function(link) {
         link.classList.remove('active');
         if (link.getAttribute('href') === '#' + current) {
             link.classList.add('active');
-            console.log('Active link:', link.getAttribute('href'));
         }
     });
 });
